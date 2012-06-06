949c0ef8d92eb9628139e90cc9d0b745133a6986
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2012-06-06 16:15:03 +0200

    Moved work directory from package directory to root and added creation of data files

---
+++ work/data/generate_fwf.cpp
@@ -0,0 +1,53 @@
+
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+  unsigned int nsim = 20;
+  unsigned int ncol = 5;
+  if (argc > 1) {
+    nsim = std::atoi(argv[1]);
+    if (argc > 2) {
+      ncol = std::atoi(argv[2]);
+    }
+  }
+
+  // create random generator
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  // create random distributions
+  std::uniform_int_distribution<> binom(0, 1);
+  std::uniform_int_distribution<> uniform_int(0, 99);
+  std::normal_distribution<> normal(0, 1);
+  std::lognormal_distribution<> lognormal(0, 1);
+  // create data
+  for(int i = 0; i < nsim; ++i) {
+
+    // id
+    std::cout << std::setw(10) << i;
+    
+    // gender
+    char gender = binom(gen) > 0 ? 'M' : 'F';
+    std::cout << gender;
+
+    // age
+    std::cout << std::setw(2) << uniform_int(gen);
+
+    // int
+    for (unsigned int j = 0; j < ncol; ++j) 
+      std::cout << std::setw(2) << uniform_int(gen);
+
+    // normal
+    for (unsigned int j = 0; j < ncol; ++j) 
+      std::cout << std::fixed << std::setw(6) << std::setprecision(2) << normal(gen);
+
+    // log-normal
+    for (unsigned int j = 0; j < ncol; ++j) 
+      std::cout << std::fixed << std::setw(6) << std::setprecision(2) << lognormal(gen);
+
+    std::cout << '\n';
+  }
+  return 0;
+}
